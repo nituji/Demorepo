e69ea4dccd028d9d2a7c33d28a069d2ebbde8ddf
Raj <ajitesh.raj@accenture.com>
2020-03-30 20:14:06 +0530

    first commit

---
+++ src/test/java/com/qa/tests/Demotest2.java
@@ -0,0 +1,17 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Demotest2 {
+		@Test
+		public void sum() {
+			System.out.println("sum");
+			int a=20;
+			int b = 30;
+			Assert.assertEquals(50, a+b);
+			
+			
+		}
+
+}
